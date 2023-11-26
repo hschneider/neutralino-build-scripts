8f5753cd2c6cc43a35e3d35822a3651316992b87
Harald Schneider <h_schneider@marketmix.com>
2023-11-26 21:52:33 +0100

    Delete dist directory

--- dist/mac_universal/ExtBunDemo.app/Contents/Resources/extensions/bun/bun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export BUN_INSTALL="$(pwd)/_runtime/"
-export PATH=${BUN_INSTALL}/bin:$PATH
-echo "-----------------------------"
-echo "Running relocated Bun runtime"
-echo "-----------------------------"
-bun "$@"
--- dist/mac_universal/ExtBunDemo.app/Contents/Resources/extensions/bun/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Copying Bun binary to the extension folder ..."
-if [ -n "$BUN_INSTALL" ]; then
-    mkdir -p _runtime/bin
-    cp $BUN_INSTALL/bin/bun _runtime/bin
-    echo "DONE :)"
-else
-    echo "ERROR: No Bun installation found :-/"
-fi
